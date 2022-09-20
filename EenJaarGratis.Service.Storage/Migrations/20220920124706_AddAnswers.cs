8e54617af98bc1df4fa9350e810117dcd3da1f22
ZD454KC <021017Winter>
2022-09-20 21:32:00 +0200

    Add migrations for answers in questions

---
+++ EenJaarGratis.Service.Storage/Migrations/20220920124706_AddAnswers.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClassLibrary1EenJaarGratis.Service.Storage.Migrations
+{
+    public partial class AddAnswers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Possibilities",
+                table: "Questions",
+                newName: "Answer3");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Answer1",
+                table: "Questions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Answer2",
+                table: "Questions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CorrectAnswer",
+                table: "Questions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Answer1",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "Answer2",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "CorrectAnswer",
+                table: "Questions");
+
+            migrationBuilder.RenameColumn(
+                name: "Answer3",
+                table: "Questions",
+                newName: "Possibilities");
+        }
+    }
+}
